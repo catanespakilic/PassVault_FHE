6a9e74ae53da69c3ee3d4d9e3a52fcd739b560f1
catanespakilic <catanespakilic@users.noreply.github.com>
2025-11-25 09:01:18 +0000

    Add complex-template.scss, components-lib-base.toml, environment-simple-demo.styl, order.scss, responsive-template-utils.rs

---
+++ mutations-components/endpoint-bridge-handler/lib-interface-helpers/responsive-template-utils.rs
@@ -0,0 +1,4 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/passvault_fhe/mutations-components/endpoint-bridge-handler/lib-interface-helpers/responsive-template-utils.rs
+// Generated at: 2025-11-27T15:10:54.974Z
+
+
